801064bd168a9551335b5826369d73ca169c916f
CindyDorantes <cindym.dorantes@gmail.com>
2023-02-09 13:33:22 -0600

    Create a view that displays a list of recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = User.create(name: 'Cindy', email: 'cindy@gmail.com', password: '123abc')
+food = Food.create(name: 'orange', price: 12, quantity: 5, measurement_unit: 'pieces', user_id: user.id)
+recipe = Recipe.create(name: 'Cake', preparation_time: '2 hours', cooking_time: '30 minutes', description: 'deliciouos cake', user_id: user.id)
+RecipeFood.create(recipe:, food:, quantity: 6)
