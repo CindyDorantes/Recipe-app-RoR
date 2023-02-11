f9de36c7d8b024d812e6a1cee780b0b8782fa9ff
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-11 23:23:23 +0300

    Reduce block for shopping list spec - fix linter errors

--- spec/integration/shopping_list_show_spec.rb
+++ spec/integration/shopping_list_show_spec.rb
@@ -2,42 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Post show page', type: :feature do
   before(:each) do
-    @user = User.create(
-      name: 'Microverse',
-      email: 'd@gmail.com',
-      password: 'password'
-    )
+    @user = User.create(name: 'Microverse', email: 'd@gmail.com', password: 'password')
 
     @food = Food.create(name: 'orange', price: 12, quantity: 5, measurement_unit: 'pieces', user: @user)
 
-    @food2 = Food.create(
-      user: @user,
-      name: 'Sardines',
-      measurement_unit: 'KGs',
-      price: 10_000,
-      quantity: 2
-    )
+    @food2 = Food.create(user: @user, name: 'Sardines', measurement_unit: 'KGs', price: 10_000, quantity: 2)
 
-    @recipe = Recipe.create(
-      user: @user,
-      name: 'Fish Marinated',
-      preparation_time: '2 HR',
-      cooking_time: '30 MIN',
-      description: 'This is',
-      public: true
-    )
+    @recipe = Recipe.create(user: @user, name: 'Fish Marinated', preparation_time: '2 HR', cooking_time: '30 MIN',
+                            description: 'This is', public: true)
 
-    @recipe_food = RecipeFood.create(
-      recipe: @recipe,
-      food: @food,
-      quantity: 3
-    )
+    @recipe_food = RecipeFood.create(recipe: @recipe, food: @food, quantity: 3)
 
-    @recipe_food2 = RecipeFood.create(
-      recipe: @recipe,
-      food: @food2,
-      quantity: 5
-    )
+    @recipe_food2 = RecipeFood.create(recipe: @recipe, food: @food2, quantity: 5)
 
     sign_in(@user)
   end
